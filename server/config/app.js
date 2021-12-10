55333fb5ffb20f38642208b05620dbfd4efc6ce2
Oscar Gonzalez <imoskgn@gmail.com>
2021-12-09 19:18:43 -0500

    Power

--- server/config/app.js
+++ server/config/app.js
@@ -1,65 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-let cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+let cors = require("cors");
+
 
 // database setup
-let mongoose = require('mongoose');
-let DB = require('./db')
+let mongoose = require("mongoose");
+let DB = require("./db");
 
-// connect mongoose 
-mongoose.connect(DB.URI, {useNewUrlParser: true, useUnifiedTopology: true})
+// connect mongoose
+mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console, "Connection Error: "))
-mongoDB.once('open', ()=>{
-  console.log('Connected to mongodb ...')
-})
-
+mongoDB.on("error", console.error.bind(console, "Connection Error: "));
+mongoDB.once("open", () => {
+  console.log("Connected to mongodb ...");
+});
 
 // add more routes here
-var tournamentRouter = require('../routes/tournament');
-var matchRouter = require('../routes/match')
-var userRouter = require('../routes/user')
-var forumRouter = require('../routes/forum')
-
+var tournamentRouter = require("../routes/tournament");
+var matchRouter = require("../routes/match");
+var userRouter = require("../routes/user");
+var forumRouter = require("../routes/forum");
 
 var app = express();
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(express.static(__dirname + '/dist'));
 
 app.use(cors());
 
-app.use('/tournament', tournamentRouter);
-app.use('/match', matchRouter)
-app.use('/user', userRouter)
-app.use('/forum', forumRouter)
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname, '../../public/index.html'))
+app.use("/tournament", tournamentRouter);
+app.use("/match", matchRouter);
+app.use("/user", userRouter);
+app.use("/forum", forumRouter);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
